cca85d8ebefe1e0c4a6e0016bc9ce7d898e8b53f
JohanSandager <jsbe@itu.dk>
2024-09-19 19:59:41 +0200

    Animating scroll indactor

--- components/IndicateScroll.vue
+++ components/IndicateScroll.vue
@@ -1,19 +1,39 @@
 <template>
-  <div
-    class="absolute top-[92%] transition delay-[20ms]"
-    :style="{ opacity: `${opacity}` }"
-  >
-    <p class="text-5xl">️⬇️</p>
+  <div class="absolute top-[92%]" :style="{ opacity: `${opacity}` }">
+    <Transition name="pop-out">
+      <p v-if="!scrolled" class="text-5xl">️⬇️</p>
+    </Transition>
   </div>
 </template>
 
 <script lang="ts" setup>
+var scrolled = ref(false);
 var opacity = useState("opacity", () => 1);
 onMounted(() => {
   window.addEventListener("scroll", () => {
-    scrollY >= 20 ? (opacity.value = 0) : (opacity.value = 1);
+    scrolled.value = scrollY >= 10 ? true : false;
   });
 });
 </script>
 
-<style></style>
+<style>
+.pop-out-enter-active {
+  animation: bounce-in 1s;
+}
+
+.pop-out-leave-to {
+  animation: bounce-in 0.5s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
