73a3ba393b129414d9e10f3b61bde666fdee6681
JohanSandager <jsbe@itu.dk>
2024-11-05 15:45:35 +0100

    fix: read apikey

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,13 @@ export default defineNuxtConfig({
 
   modules: ['@nuxtjs/supabase'],
 
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL || '',
+      supabaseKey: process.env.SUPABASE_KEY || ''
+    }
+  },
+
   supabase: {
     redirect: false
   }
