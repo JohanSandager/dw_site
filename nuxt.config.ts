b31480ba82477cd86e61af4bd9d4193b73761495
JohanSandager <jsbe@itu.dk>
2024-11-05 11:27:47 +0100

    feat: add Supabase for Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+
   app: {
     head: {
       link: [{rel: 'icon', type: 'image/png', href: '/logo.png'}]
     }
   },
+
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     }
-  }
+  },
+
+  modules: ['@nuxtjs/supabase']
 })
